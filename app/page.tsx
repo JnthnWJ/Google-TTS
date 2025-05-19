b047f8e59813bd3d84ee7386ae84f7ae4d48e78e
JnthnWJ <jnthnwj@gmail.com>
2025-05-19 12:42:03 -0700

    Fix the background

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="relative min-h-screen w-full overflow-hidden">
       <div className="fixed inset-0 -z-10">
         <Image
-          src="https://i0.wp.com/mrmacintosh.com/wp-content/uploads/2024/08/Screenshot-2024-08-05-at-5.37.59%E2%80%AFPM.png?w=1680&ssl=1"
+          src="/background.png"
           alt="Apple Vision Pro Background"
           fill
           className="object-cover"
